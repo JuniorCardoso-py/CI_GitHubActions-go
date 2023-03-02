b153253889e4639929bc54c2c72b5de06600a108
Tarcisio Cardoso Junior <50811299+JuniorCardoso-py@users.noreply.github.com>
2023-03-01 23:04:08 -0300

    Develop (#10)
    * Adicionado stage para develop branch (#3)
    
    * Test
    
    * Test2
    
    * Test (#7)
    
    * Adicionando stage para main (#9)
    
    * Test
    
    * Testando_strategys
    
    * fix?
    
    * fix2?
    
    * :art: RealFix
    
    * Added more comments

--- anotherpackage/model.go
+++ anotherpackage/model.go
@@ -1,10 +1,12 @@
 package user_adapter
 
+// User struct
 type User struct {
 	Name string
 	Age  int
 }
 
+// CreatUser is used to create user
 func CreateUser(name string, age int) *User {
 	return &User{
 		Name: name,
