d46559c039ee18f53e371475aaa1026053f3fe7d
JuniorCardoso-py <juniorcardoso.sc@outlook.com>
2023-03-19 21:52:48 -0300

    Added some configs

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /app
 
 COPY . .
 
-RUN go mod init github.com/golangci_test
 RUN go mod tidy 
 RUN go build -o math 
 
--- anotherpackage/model.go
+++ anotherpackage/model.go
@@ -5,15 +5,3 @@ type User struct {
 	Name string
 	Age  int
 }
-
-// CreatUser is used to create user
-func CreateUser(name string, age int) *User {
-	return &User{
-		Name: name,
-		Age:  age,
-	}
-}
-
-func (u *User) GetUser() *User {
-	return u
-}
